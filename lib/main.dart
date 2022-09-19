a7785e4286bfb92201f08c7aa3e9896abf0059c8
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2022-09-19 12:25:55 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'dart:async';
 import 'dart:convert';
 
 void main() async {
-  print(await getData());
   runApp(MaterialApp(
-    home: Container(),
+    home: const Home(),
+    theme: ThemeData(hintColor: Colors.amber, primaryColor: Colors.white),
   ));
 }
 
@@ -14,4 +14,78 @@ Future<Map> getData() async {
   final response = await http
       .get(Uri.parse('https://api.hgbrasil.com/finance?key=f2b7824c'));
   return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(60, 253, 252, 252),
+      appBar: AppBar(
+        title: const Text('\$ Conversor \$'),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return const Center(
+                  child: Text('Carregando Dados...',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 25.0,
+                      ),
+                      textAlign: TextAlign.center),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text('Erro ao Carregar Dados...',
+                        style: TextStyle(
+                          color: Colors.amber,
+                          fontSize: 25.0,
+                        ),
+                        textAlign: TextAlign.center),
+                  );
+                } else {
+                  var dolar =
+                      snapshot.data!['results']['currencies']['USD']['buy'];
+                  var euro =
+                      snapshot.data!['results']['currencies']['EUR']['buy'];
+                  return SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const Icon(
+                          Icons.monetization_on,
+                          size: 150.0,
+                          color: Colors.amber,
+                        ),
+                        const TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Reais',
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: 'R\$'),
+                          style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                        )
+                      ],
+                    ),
+                  );
+                }
+            }
+          }),
+    );
+  }
 }
